5f83355642290531f13741472d198a45c7844e53
kswiiz0 <kswiiz0@gmail.com>
2020-06-17 21:05:56 +0900

    회원가입할때, 닉네임을 수정하면 다시 중복검사를 할수 있도록 재검사를 요구하는 기능 추가

--- app/src/main/java/com/phis/apipractice_20200615/SignUpActivity.kt
+++ app/src/main/java/com/phis/apipractice_20200615/SignUpActivity.kt
@@ -30,7 +30,7 @@ class SignUpActivity : BaseActivity() {
         var isEmailOk = false
         var isNickNameOk = false
 
-        emailEdt.addTextChangedListener(object : TextWatcher{
+        emailEdt.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
             }
@@ -41,13 +41,12 @@ class SignUpActivity : BaseActivity() {
 
             //문구가 바꼈을때
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("바뀐이메일",s.toString())
+                Log.d("바뀐이메일", s.toString())
                 emailCheckResultTxt.text = "이메일 중복검사를 해주세요."
                 isEmailOk = false
             }
 
         })
-
 
         emailCheckBtn.setOnClickListener {
             val email = emailEdt.text.toString()
@@ -75,6 +74,25 @@ class SignUpActivity : BaseActivity() {
                     }
                 })
         }
+
+
+        nickNameEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            //문구가 바꼈을때
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("바뀐닉네임", s.toString())
+                nickNameCheckResultTxt.text = "닉네임 중복검사를 해주세요."
+                isNickNameOk = false
+            }
+
+        })
 
 
         nickNameBtn.setOnClickListener {
@@ -107,19 +125,19 @@ class SignUpActivity : BaseActivity() {
         }
         signupBtn.setOnClickListener {
 
-            if ( !isEmailOk){
+            if (!isEmailOk) {
                 Toast.makeText(mContext, "이메일 중복검사를 통과해야 합니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            if ( !isNickNameOk){
+            if (!isNickNameOk) {
                 Toast.makeText(mContext, "닉네임 중복검사를 통과해야 합니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
 
             val password = passwordEdt.text.toString().trim()
-            if ( password.length < 8 ){
+            if (password.length < 8) {
                 Toast.makeText(mContext, "비밀번호는 8자리 이상이어야합니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
