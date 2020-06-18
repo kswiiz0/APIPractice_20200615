81caeabb2934df1a30f4f8e0c1bba505359dd999
kswiiz0 <kswiiz0@gmail.com>
2020-06-18 19:52:52 +0900

    주제의 내용중 선택진영들 파싱 작업중 ->선택 진영도 일종의 클래스화 (파싱 기능 작업)

--- app/src/main/java/com/phis/apipractice_20200615/datas/Topic.kt
+++ app/src/main/java/com/phis/apipractice_20200615/datas/Topic.kt
@@ -7,20 +7,36 @@ class Topic {
     var id = 0
     var title = ""
     var imageUrl = ""
+    var sides = ""
 
 
     companion object {
         fun getTopicFromJson(json: JSONObject): Topic {
 /*
-            "id":2,
-            "title":"민트는 음식인가 아닌가",
-            "img_url":"https:\/\/t1.daumcdn.net\/cfile\/tistory\/217A9546576F76690C",s
+            topic":{
+                 "id":2,
+                 "title":"민트는 음식인가 아닌가",
+                 "img_url":"https://t1.daumcdn.net/cfile/tistory/217A9546576F76690C",
+                 "start_date":"2020-05-10 00:00:00",
+                 "end_date":"2021-05-16 23:59:59",
+                 "sides":[],
+                 "my_side_id":-1,
+                 "replies":[],
+                 "reply_count":13
+            }
 */
 
             val t = Topic()
             t.id = json.getInt("id")
             t.title = json.getString("title")
             t.imageUrl = json.getString("img_url")
+
+            val sides = json.getJSONObject("sides")
+            for (i in 0..sides.length() - 1) {
+                
+            }
+
+
 
             return t
         }
---
+++ app/src/main/java/com/phis/apipractice_20200615/datas/TopicSide.kt
@@ -0,0 +1,34 @@
+package com.phis.apipractice_20200615.datas
+
+import org.json.JSONObject
+
+class TopicSide {
+/*
+    "id": 5,
+    "topic_id": 2,
+    "title": "음식이 맞다",
+    "vote_count": 9,
+    "best_reply": null
+*/
+
+    var id = 0
+    var topicId = 0
+    var title = ""
+    var voteCount = 0
+
+    companion object {
+        fun getTopicSideFromJson(json: JSONObject): TopicSide {
+            var ts = TopicSide()
+            ts.id = json.getInt("id")
+            ts.topicId = json.getInt("topic_id")
+            ts.title = json.getString("title")
+            ts.voteCount = json.getInt("vote_count")
+
+
+
+            return ts
+        }
+    }
+
+
+}
