853c50b44ce0d18b7b78f3946578158036076f6f
kswiiz0 <kswiiz0@gmail.com>
2020-06-24 19:47:23 +0900

    커스텀 액션바 배경색 제거

--- app/src/main/java/com/phis/apipractice_20200615/MainActivity.kt
+++ app/src/main/java/com/phis/apipractice_20200615/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.phis.apipractice_20200615
 
+
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.phis.apipractice_20200615.adapters.TopicAdapter
 import com.phis.apipractice_20200615.datas.Topic
 import com.phis.apipractice_20200615.utils.ContextUtil
@@ -22,6 +24,8 @@ class MainActivity : BaseActivity() {
 
         setValues()
         setupEvent()
+
+        setCustomActionBar()
     }
 
     override fun setValues() {
@@ -94,14 +98,30 @@ class MainActivity : BaseActivity() {
 
             val clickedTopic = topicList[position]
 
-            val myIntent = Intent(mContext,ViewTopicDetailActivity::class.java)
+            val myIntent = Intent(mContext, ViewTopicDetailActivity::class.java)
             myIntent.putExtra("topic_id", clickedTopic.id)
             startActivity(myIntent)
         }
 
 
-
     }
 
+    fun setCustomActionBar() {
+        //상단배너 커스텀
+
+        //커스텀 기능 활성화
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.custom_action_bar)
+
+        //기본 배경색 제거
+        supportActionBar?.setBackgroundDrawable(null)
+
+        //커스트텀 액션바 영역 확장 > 뒷단 배경 삭제
+        val parent = supportActionBar?.customView?.parent as androidx.appcompat.widget.Toolbar
+        
+        //실제 여백 제거
+        parent.setContentInsetsAbsolute(0,0)
+
+    }
 
 }
