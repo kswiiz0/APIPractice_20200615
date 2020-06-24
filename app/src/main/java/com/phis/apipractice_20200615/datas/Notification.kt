2abe274ef52c4f27b71b0f711a3402af8647ab2e
kswiiz0 <kswiiz0@gmail.com>
2020-06-24 21:19:02 +0900

    알림목록 가져올때 시간을 가져오기 > 시차 반영

--- app/src/main/java/com/phis/apipractice_20200615/datas/Notification.kt
+++ app/src/main/java/com/phis/apipractice_20200615/datas/Notification.kt
@@ -1,6 +1,7 @@
 package com.phis.apipractice_20200615.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 
 class Notification {
@@ -21,13 +22,28 @@ class Notification {
             val noti = Notification()
 
             noti.id = json.getInt("id")
-            noti.receiveUserId  = json.getInt("receive_user_id")
+            noti.receiveUserId = json.getInt("receive_user_id")
             noti.actUserId = json.getInt("act_user_id")
             noti.title = json.getString("title")
             noti.type = json.getString("type")
             noti.message = json.getString("message")
             noti.referenceUi = json.getString("reference_ui")
             noti.focusObjectId = json.getInt("focus_object_id")
+
+            //서버의 String 시간 > Kotlin Date 으로 반영.
+            val createdAt = json.getString("created_at")
+            val parsingFormat = SimpleDateFormat("YYYY-MM-DD HH:mm:ss")
+
+            noti.createAt.time = parsingFormat.parse(createdAt)
+
+            //시차보정
+            val myPhoneTimeZone = noti.createAt.timeZone
+            val timeOffset = myPhoneTimeZone.rawOffset / 1000 / 60 / 60
+            noti.createAt.add(Calendar.HOUR, timeOffset)
+
+
+
+
 
             return noti
         }
