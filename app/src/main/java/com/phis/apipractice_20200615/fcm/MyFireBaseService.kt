594bd11593415c6440484027df6dd5051194416b
kswiiz0 <kswiiz0@gmail.com>
2020-06-25 21:07:06 +0900

    푸쉬알람 수신이벤트 처리용 클래스를 생성, 매니패스트에 등록처리

--- app/src/main/java/com/phis/apipractice_20200615/fcm/MyFireBaseService.kt
+++ app/src/main/java/com/phis/apipractice_20200615/fcm/MyFireBaseService.kt
@@ -2,8 +2,9 @@ package com.phis.apipractice_20200615.fcm
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class MyFireBaseService : FirebaseMessagingService(){
+class MyFireBaseService : FirebaseMessagingService() {
 
     //토큰이 새로 발급되면 실행되는 함수. ( 보통 앱 최초 실행시 2~3초 이후에 실행됨)
     override fun onNewToken(p0: String) {
@@ -11,6 +12,15 @@ class MyFireBaseService : FirebaseMessagingService(){
 
         Log.d("새토큰 발급", p0)
 
+
+    }
+
+
+    //실제 푸쉬알림 수신시 실행되는 함수
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+
+        Log.d("새토큰 발급", p0.notification?.title)
 
     }
 
